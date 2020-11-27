1f8840287aa5bba3cebcbc6b2c4c6001904c641d
Julia Maria <J.Brenner@campus.lmu.de>
2020-11-27 13:42:11 +0100

    Add bottom navigation functionality.

--- Android App/app/src/main/java/com/example/findyourngo/MainActivity.kt
+++ Android App/app/src/main/java/com/example/findyourngo/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.findyourngo
 
 import android.os.Bundle
+import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.findyourngo.ui.favourites.FavouritesFragment
+import com.example.findyourngo.ui.maps.MapsFragment
+import com.example.findyourngo.ui.overview.OverviewFragment
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,13 +20,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener(navigationItemSelectedListener)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_maps, R.id.navigation_overview, R.id.navigation_favourites))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        if (savedInstanceState == null) {
+            val fragment = MapsFragment()
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                    .commit()
+        }
+    }
+
+    private val navigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+        when (menuItem.itemId) {
+            R.id.navigation_maps -> {
+                val fragment = MapsFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_overview -> {
+                val fragment = OverviewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_favourites -> {
+                val fragment = FavouritesFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
     }
 }
