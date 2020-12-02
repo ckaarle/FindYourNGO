52abd894c8d365f6dc2ffc346746d30454ecf453
Julia Maria <J.Brenner@campus.lmu.de>
2020-12-02 10:28:06 +0100

    Add ngo serializers.

---
+++ Backend/findyourngo/restapi/serializers/ngo_serializer.py
@@ -0,0 +1,98 @@
+from rest_framework import serializers
+from findyourngo.restapi.models import Ngo, NgoAddress, NgoContact, NgoDataSource, NgoRepresentative, NgoMetaData, \
+    NgoStats
+
+
+class NgoDataSourceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NgoDataSource
+        fields = ['credible', 'source']
+
+
+class NgoAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NgoAddress
+        fields = ['street', 'postcode', 'city', 'country']
+
+
+class NgoRepresentativeSerializer(serializers.ModelSerializer):
+    representativeFirstName = serializers.CharField(source='representative_first_name')
+    representativeLastName = serializers.CharField(source='representative_last_name')
+    representativeEmail = serializers.EmailField(source='representative_email')
+
+    class Meta:
+        model = NgoRepresentative
+        fields = ['representativeFirstName', 'representativeLastName', 'representativeEmail']
+
+
+class NgoStatsSerializer(serializers.ModelSerializer):
+    typeOfOrganization = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='type',
+        source='type_of_organization'
+    )
+
+    foundingYear = serializers.IntegerField(source='founding_year')
+    staffNumber = serializers.IntegerField(source='staff_number')
+    memberNumber = serializers.IntegerField(source='member_number')
+    workingLanguages = serializers.CharField(source='working_languages')
+    presidentFirstName = serializers.CharField(source='president_first_name')
+    presidentLastName = serializers.CharField(source='president_last_name')
+    yearlyIncome = serializers.CharField(source='yearly_income')
+
+    class Meta:
+        model = NgoStats
+        fields = ['foundingYear', 'staffNumber', 'memberNumber', 'workingLanguages', 'funding',
+                  'presidentFirstName', 'presidentLastName', 'typeOfOrganization',
+                  'yearlyIncome']
+
+
+class NgoContactSerializer(serializers.ModelSerializer):
+    ngoPhoneNumber = serializers.CharField(source='ngo_phone_number')
+    ngoEmail = serializers.EmailField(source="ngo_email")
+    address = NgoAddressSerializer(read_only=True)
+    representative = NgoRepresentativeSerializer(read_only=True)
+
+    class Meta:
+        model = NgoContact
+        fields = ['ngoPhoneNumber', 'ngoEmail', 'website', 'address', 'representative']
+
+
+class NgoMetaDataSerializer(serializers.ModelSerializer):
+    lastUpdated = serializers.DateField(source='last_updated')
+    infoSource = NgoDataSourceSerializer(read_only=True, many=True, source='info_source')
+
+    class Meta:
+        model = NgoMetaData
+        fields = ['lastUpdated', 'infoSource']
+
+
+class NgoSerializer(serializers.ModelSerializer):
+    branches = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='country'
+    )
+
+    topics = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='topic'
+    )
+
+    accreditations = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='accreditation'
+    )
+
+    stats = NgoStatsSerializer(read_only=True)
+    contact = NgoContactSerializer(read_only=True)
+    metaData = NgoMetaDataSerializer(source='meta_data', read_only=True)
+
+    class Meta:
+        model = Ngo
+        fields = ['id', 'name', 'acronym', 'aim', 'activities', 'branches', 'topics', 'accreditations', 'stats',
+                  'contact',
+                  'metaData']
