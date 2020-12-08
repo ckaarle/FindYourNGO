b85f73f5c6a0da92e345edd5ccb2248b96abd5b0
Julia Maria <J.Brenner@campus.lmu.de>
2020-12-08 10:18:06 +0100

    Delete Android files.

--- Android App/app/src/main/java/com/example/findyourngo/models/NGOItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.findyourngo.models
-
-class NGOItem(val name: String,
-              val siteOfOperation: String,
-              val value: Float,
-              val amount: Int) {
-}
