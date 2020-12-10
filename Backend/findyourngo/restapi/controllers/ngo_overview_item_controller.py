95d54ccbc11207ab78fdaaf408c89cd1517cb089
Chris <karle.christina@web.de>
2020-12-10 16:55:57 +0100

    Change ngo overview item list to class to enable easy paginating.

--- Backend/findyourngo/restapi/controllers/ngo_overview_item_controller.py
+++ Backend/findyourngo/restapi/controllers/ngo_overview_item_controller.py
@@ -1,13 +1,15 @@
+from typing import Any
+
 from django.http.response import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.generics import GenericAPIView
+
 from findyourngo.restapi.models import Ngo
 from findyourngo.restapi.serializers.ngo_overview_item_serializer import NgoOverviewItemSerializer
 
 
-@api_view(['GET'])
-def ngo_overview_item_list(request):
-    if request.method == 'GET':
+class NgoOverviewItemList(GenericAPIView):
+
+    def get(self, request: Any) -> JsonResponse:
         ngo_overview_items = Ngo.objects.all()
         ngo_overview_item_serializer = NgoOverviewItemSerializer(ngo_overview_items, many=True)
         return JsonResponse(ngo_overview_item_serializer.data, safe=False)
-
