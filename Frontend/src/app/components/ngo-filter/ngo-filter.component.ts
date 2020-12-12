163618371463d83916d32f09e95633245261f7c1
Julia Maria <J.Brenner@campus.lmu.de>
2020-12-12 19:24:29 +0100

    Add filtering ui draft.

---
+++ Frontend/src/app/components/ngo-filter/ngo-filter.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { NgoFilterOptions } from 'src/app/models/ngo';
+import { FilterService } from 'src/app/services/filter.service';
+
+@Component({
+  selector: 'ngo-filter',
+  templateUrl: './ngo-filter.component.html',
+  styleUrls: ['./ngo-filter.component.scss']
+})
+export class NgoFilterComponent implements OnInit {
+  filtersAvailable: boolean = false;
+  filterOptions: NgoFilterOptions = {} as NgoFilterOptions;
+  filterSelection: NgoFilterOptions = {} as NgoFilterOptions;
+
+  constructor(private filter: FilterService) { }
+
+  ngOnInit(): void {
+    this.getFilterOptions();
+  }
+
+  getFilterOptions() {
+    this.filter.getNgoFilterOptions().subscribe(data => {
+      this.filterOptions = data;
+      this.filtersAvailable = true;
+    })
+  }
+
+  isStringArray(value: any): boolean {
+    return Array.isArray(value);
+  }
+
+  isBoolean(value: any): boolean {
+    return typeof value === 'boolean';
+  }
+
+  addValue(keyOption: any, value: any, multiple: boolean = false) { //WiP
+    if (this.filterSelection[keyOption] && multiple) {
+      this.filterSelection[keyOption].values.push(value);
+    } else {
+      if (multiple) {
+        this.filterSelection[keyOption] = { values: [value] };
+      } else {
+        this.filterSelection[keyOption] = { values: value }
+      }
+    }
+  }
+
+  applyFilter() { //WiP
+    console.log("FilterSelection: ", this.filterSelection);
+    this.filter.applyFilter(this.filterSelection).subscribe(data => {
+      console.log("FilteredNgoItems: ", data); //TODO: display result
+    });
+  }
+
+}
