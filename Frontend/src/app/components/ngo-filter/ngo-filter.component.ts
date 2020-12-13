5f2acb1ef1da764482e938b384aff0544ab77d1e
Julia Maria <J.Brenner@campus.lmu.de>
2020-12-13 09:53:40 +0100

    Edit frontend filter option object.

--- Frontend/src/app/components/ngo-filter/ngo-filter.component.ts
+++ Frontend/src/app/components/ngo-filter/ngo-filter.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgoFilterOptions } from 'src/app/models/ngo';
 import { FilterService } from 'src/app/services/filter.service';
 
+export interface NgoFilterSelection {}
+
 @Component({
   selector: 'ngo-filter',
   templateUrl: './ngo-filter.component.html',
@@ -10,7 +12,7 @@ import { FilterService } from 'src/app/services/filter.service';
 export class NgoFilterComponent implements OnInit {
   filtersAvailable: boolean = false;
   filterOptions: NgoFilterOptions = {} as NgoFilterOptions;
-  filterSelection: NgoFilterOptions = {} as NgoFilterOptions;
+  filterSelection: NgoFilterSelection = {} as NgoFilterSelection;
 
   constructor(private filter: FilterService) { }
 
@@ -35,12 +37,12 @@ export class NgoFilterComponent implements OnInit {
 
   addValue(keyOption: any, value: any, multiple: boolean = false) { //WiP
     if (this.filterSelection[keyOption] && multiple) {
-      this.filterSelection[keyOption].values.push(value);
+      this.filterSelection[keyOption].push(value);
     } else {
       if (multiple) {
-        this.filterSelection[keyOption] = { values: [value] };
+        this.filterSelection[keyOption] = [value];
       } else {
-        this.filterSelection[keyOption] = { values: value }
+        this.filterSelection[keyOption] = value;
       }
     }
   }
--- Frontend/src/app/services/filter.service.ts
+++ Frontend/src/app/services/filter.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { NgoFilterOptions, NgoOverviewItem } from '../models/ngo';
+import { NgoOverviewItem } from '../models/ngo';
 import { Observable } from 'rxjs';
-import { NgoOverviewItemComponent } from '../components/ngo-overview-item/ngo-overview-item.component';
+import { NgoFilterSelection } from '../components/ngo-filter/ngo-filter.component';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +16,7 @@ export class FilterService {
     return this.http.get<NgoFilterOptions>(endpoint); //TODO: move this to request service
   }
 
-  applyFilter(filterSelection: NgoFilterOptions): Observable<NgoOverviewItem[]> {
+  applyFilter(filterSelection: NgoFilterSelection): Observable<NgoOverviewItem[]> {
     let endpoint = 'http://localhost:8000/ngos/filter/';
     return this.http.post<NgoOverviewItem[]>(endpoint, filterSelection);
   }
