1da04c12c2c3eaf2a4ed17a4798af4356cf51cc6
Chris Karle <christina.karle@yahoo.de>
2020-12-18 12:58:23 +0100

    Fix one merge error.

--- Backend/findyourngo/restapi/controllers/ngo_controller.py
+++ Backend/findyourngo/restapi/controllers/ngo_controller.py
@@ -1,10 +1,16 @@
+from typing import Any
+
+from django.db.models import QuerySet
 from django.http.response import JsonResponse
 
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework import status
 
-from findyourngo.restapi.models import Ngo
+from findyourngo.filtering.NgoFilter import NgoFilter
+from findyourngo.restapi.models import Ngo, NgoTopic, NgoBranch, NgoAddress, NgoType, NgoStats
+from findyourngo.restapi.serializers.ngo_overview_item_serializer import NgoOverviewItemSerializer
+from findyourngo.restapi.serializers.filter_serializer import FilterSerializer, filter_object
 from findyourngo.restapi.serializers.ngo_serializer import NgoSerializer
 
 
@@ -48,3 +54,51 @@ def ngo_detail(request, pk):
         return JsonResponse({'message': 'Ngo was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
 
 
+@api_view(['GET'])
+def ngo_filter_options(request: Any) -> JsonResponse:
+    return JsonResponse(filter_object())
+
+def filter_object():  # TODO: move to frontend
+    return {
+        'branches': {"displayName": "Branches", "values": branches(), "icon": "account_tree"},
+        'topics': {"displayName": "Topics", "values": topics(), "icon": "topic"},
+        'hasEcosoc': {"displayName": "Accreditations", "values": False, "icon": "account_balance"},
+        'isCredible': {"displayName": "Credibility", "values": False, "icon": "loyalty"},
+        'countries': {"displayName": "Countries", "values": hq_countries(), "icon": "flag"},
+        'cities': {"displayName": "Cities", "values": None, "icon": "location_on"},
+        'contactOptionPresent': {"displayName": "Contactable", "values": False, "icon": "how_to_reg"},
+        'typeOfOrganization': {"displayName": "Type of organization", "values": types_of_organization(),
+                               "icon": "corporate_fare"},
+        'workingLanguages': {"displayName": "Working languages", "values": working_languages(),
+                             "icon": "translate"},
+        'funding': {"displayName": "Funding", "values": funding(), "icon": "attach_money"},
+        'trustworthiness': {"displayName": "Trustworthiness", "values": None, "icon": "star"}
+    }
+
+def branches():
+    branches = list(map(lambda ngo_branch: ngo_branch['country'],
+                        NgoBranch.objects.all().order_by('country').values('country').distinct()))
+    return branches
+
+def topics():
+    topics = list(map(lambda ngo_topic: ngo_topic['topic'],
+                      NgoTopic.objects.all().order_by('topic').values('topic').distinct()))
+    return topics
+
+def hq_countries():
+    hq_countries = list(map(lambda ngo_hq_address: ngo_hq_address['country'],
+                            NgoAddress.objects.all().order_by('country').values('country').distinct()))
+    return hq_countries
+
+def types_of_organization():
+    types_of_organization = list(
+        map(lambda ngo_type: ngo_type['type'], NgoType.objects.all().order_by('type').values('type').distinct()))
+    return types_of_organization
+
+def working_languages():
+    return ["English", "French", "German"]
+
+def funding():
+    funding = list(map(lambda ngo_stats: ngo_stats['funding'],
+                       NgoStats.objects.all().order_by('funding').values('funding').distinct()))
+    return funding
