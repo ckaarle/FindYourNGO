ae9375c3ddd11fedea87114466185a0e86650bb8
Julia Maria <J.Brenner@campus.lmu.de>
2020-12-19 17:41:52 +0100

    Add ngo detail interface.

--- Frontend/src/app/models/ngo.ts
+++ Frontend/src/app/models/ngo.ts
@@ -1,12 +1,56 @@
 // add ngo interfaces here
 
+export interface NgoDetailItem {
+  id: number;
+  name: string;
+  acronym: string;
+  aim: string;
+  activities: string;
+  branches: string[];
+  topics: string[];
+  accreditations: string[];
+  stats: NgoStats;
+  contact: NgoContact;
+  trustworthiness: number;
+  amount: number; //TODO
+}
+
+export interface NgoStats {
+  foundingYear: number;
+  staffNumber: number;
+  memberNumber: number;
+  workingLanguages: string;
+  funding: string;
+  presidentFirstName: string;
+  presidentLastName: string;
+  yearlyIncome: string;
+  typeOfOrganization: string[];
+}
+
+export interface NgoContact {
+  ngoPhoneNumber: string;
+  ngoEmail: string;
+  website: string;
+  representative: {
+    representativeFirstName: string;
+    representativeLastName: string;
+    representativeEmail: string;
+  };
+  address: {
+    street: string;
+    postcode: string;
+    city: string;
+    country: string;
+  }
+}
+
 export interface NgoOverviewItem {
   id: number;
   name: string;
   acronym: string;
   city: number;
   trustworthiness: number;
-  amount: number;
+  amount: number; //TODO
 }
 
 export interface NgoOverviewItemPagination {
