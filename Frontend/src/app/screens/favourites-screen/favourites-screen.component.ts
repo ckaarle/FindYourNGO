8eebf22a210e650644cad61dc1e934eaca8216a6
Chris Karle <christina.karle@yahoo.de>
2021-01-25 19:47:36 +0100

    Favourites screen only accessible to logged in users.

--- Frontend/src/app/screens/favourites-screen/favourites-screen.component.ts
+++ Frontend/src/app/screens/favourites-screen/favourites-screen.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ApiService} from '../../services/api.service';
+import {MatDialog} from '@angular/material/dialog';
+import {LoginDialogComponent} from '../login-dialog/login-dialog.component';
 
 @Component({
   selector: 'app-favourites-screen',
@@ -8,10 +10,20 @@ import {ApiService} from '../../services/api.service';
 })
 export class FavouritesScreenComponent implements OnInit {
 
-  constructor(apiService: ApiService) {
+  userLoggedIn: boolean = false;
+
+  constructor(private apiService: ApiService, private dialog: MatDialog) {
   }
 
   ngOnInit(): void {
+    this.userLoggedIn = this.apiService.userid.getValue();
+    this.apiService.userid.subscribe((id: string) => {
+      this.userLoggedIn = id !== '';
+    });
+
+    if (!this.userLoggedIn) {
+      this.dialog.open(LoginDialogComponent);
+    }
   }
 
 }
