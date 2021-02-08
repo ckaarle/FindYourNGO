d26b3d2d6ae353c7c198c4fdc85b54559e066736
Chris Karle <christina.karle@yahoo.de>
2021-02-08 20:46:02 +0100

    Add custom service to make sure the about page actually scrolls to the tw score explanation ...

--- Frontend/src/app/screens/about/about.component.ts
+++ Frontend/src/app/screens/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AutoscrollService} from '../../services/autoscroll.service';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private autoscrollService: AutoscrollService) { }
 
   ngOnInit(): void {
+    this.autoscrollService.listen();
   }
 
 }
