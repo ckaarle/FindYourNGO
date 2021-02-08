303964a447f2b725e816dc410df504c7362ec8bd
Chris Karle <christina.karle@yahoo.de>
2021-02-08 20:22:51 +0100

    Add reference to the about page to the overview.
    Since html in tooltips is not supported, it is only a textual reference.
    
    ad

--- Frontend/src/app/shared.module.ts
+++ Frontend/src/app/shared.module.ts
@@ -24,6 +24,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 import {MatChipsModule} from '@angular/material/chips';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -91,7 +92,8 @@ FullCalendarModule.registerPlugins([ // register FullCalendar plugins
     FlexLayoutModule,
     AppRoutingModule,
     MatAutocompleteModule,
-    FullCalendarModule
+    FullCalendarModule,
+    MatTooltipModule
   ],
   declarations: [
     OverviewScreenComponent,
@@ -140,7 +142,8 @@ FullCalendarModule.registerPlugins([ // register FullCalendar plugins
     MatProgressBarModule,
     FormsModule,
     ReactiveFormsModule,
-    UserOptionsComponent
+    UserOptionsComponent,
+    MatTooltipModule
   ],
   providers: [
     MediaService,
