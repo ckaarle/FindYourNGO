abdc03b7d98abd4f518d2f3c5c74ade870470d94
Chris Karle <christina.karle@yahoo.de>
2021-02-20 10:34:11 +0100

    Add new component for NGO sign up.

---
+++ Frontend/src/app/components/ngo-sign-up/ngo-sign-up.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngo-sign-up',
+  templateUrl: './ngo-sign-up.component.html',
+  styleUrls: ['./ngo-sign-up.component.scss']
+})
+export class NgoSignUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
