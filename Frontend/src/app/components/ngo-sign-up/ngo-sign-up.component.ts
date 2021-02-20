fc52d21575fbd9169c7b70c145a7e8b221a7b995
Chris Karle <christina.karle@yahoo.de>
2021-02-20 12:23:30 +0100

    Add form to input new ngo information plus call to backend.

--- Frontend/src/app/components/ngo-sign-up/ngo-sign-up.component.ts
+++ Frontend/src/app/components/ngo-sign-up/ngo-sign-up.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {NgoRegistrationService} from '../../services/ngo-registration.service';
+import {UserService} from '../../services/user.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {NewNgo} from '../../models/ngo';
 
 @Component({
   selector: 'app-ngo-sign-up',
@@ -7,9 +12,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgoSignUpComponent implements OnInit {
 
-  constructor() { }
+  group = new FormGroup({
+    ngoNameControl: new FormControl('', Validators.required),
+    countryControl: new FormControl('', Validators.required),
+
+    firstNameControl: new FormControl('', Validators.required),
+    lastNameControl: new FormControl('', Validators.required),
+    emailRepresentativeControl: new FormControl('', [Validators.required, Validators.email]),
+
+    userForm: new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    })
+  });
+
+
+  constructor(
+      private ngoRegistrationService: NgoRegistrationService,
+      private userService: UserService,
+      private snackBar: MatSnackBar
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
+  submit(): void {
+    const newNgo = this.convertToNgo();
+
+    this.ngoRegistrationService.registerNewNgo(newNgo).subscribe((result: any) => {
+          const query = {ngo_name: this.group.get('ngoNameControl')?.value};
+          // this.userService.register(this.group.get('userForm').value, query); TODO put back in
+        },
+        (error: any) => {
+          const userMessage = error.message;
+          this.snackBar.open(userMessage, '', {
+            duration: 3000,
+            panelClass: ['login-snackbar']
+          });
+        });
+  }
+
+  private convertToNgo(): NewNgo {
+    return {
+      ngoName: this.group.get('ngoNameControl')?.value,
+      ngoCountry: this.group.get('countryControl')?.value,
+
+      representativeFirstName: this.group.get('firstNameControl')?.value,
+      representativeLastName: this.group.get('lastNameControl')?.value,
+      representativeEmail: this.group.get('emailRepresentativeControl')?.value
+    };
+  }
 }
---
+++ Frontend/src/app/services/ngo-registration.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {NewNgo} from '../models/ngo';
+import {Observable} from 'rxjs';
+import {ApiService} from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NgoRegistrationService {
+
+  constructor(private apiService: ApiService) { }
+
+  registerNewNgo(newNgo: NewNgo): Observable<any> {
+    return this.apiService.post('registerNgo', {}, {ngo: newNgo});
+  }
+}
