08c3b133788013cef6c9017bfe528d08a78578fa
Chris Karle <christina.karle@yahoo.de>
2021-02-20 14:44:44 +0100

    Show success and error messages for ngo registration.

--- Frontend/src/app/components/ngo-sign-up/ngo-sign-up.component.ts
+++ Frontend/src/app/components/ngo-sign-up/ngo-sign-up.component.ts
@@ -4,6 +4,7 @@ import {NgoRegistrationService} from '../../services/ngo-registration.service';
 import {UserService} from '../../services/user.service';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import {NewNgo} from '../../models/ngo';
+import {BehaviorSubject} from 'rxjs';
 
 @Component({
   selector: 'app-ngo-sign-up',
@@ -27,39 +28,57 @@ export class NgoSignUpComponent implements OnInit {
     })
   });
 
+  $errorMessage = new BehaviorSubject<string>('');
+
+  registrationSuccessful = false;
 
   constructor(
       private ngoRegistrationService: NgoRegistrationService,
       private userService: UserService,
-      private snackBar: MatSnackBar
+      private snackBar: MatSnackBar,
   ) {
   }
 
   ngOnInit(): void {
+    this.$errorMessage.subscribe(value => {
+      if (value !== '') {
+        this.showErrorMessage();
+      }
+    });
+    this.userService.$lastErrorMessage.subscribe(errorMessage => this.$errorMessage.next(errorMessage));
+
     // TODO fetch all ngo names and throw error if one of them is used via a validator (plus message!)
     // TODO country autocomplete
   }
 
   submit(): void {
-    const newNgo = this.convertToNgo();
+    const newNgo = this.convertFormInputToNgo();
+
+    this.$errorMessage.next('');
 
     this.ngoRegistrationService.registerNewNgo(newNgo).subscribe((result: any) => {
           const query = {ngo_name: this.group.get('ngoNameControl')?.value};
-          // this.userService.register(this.group.get('userForm').value, query); TODO put back in
-          console.log(result.message)
+          this.userService.register(this.group.get('userForm')?.value, query);
+          this.userService.signOut(); // validate account first
 
-      // TODO redirect back to overview
+          if (this.$errorMessage.getValue() === '') {
+            this.registrationSuccessful = true;
+          }
         },
         (error: any) => {
           const userMessage = error.error.error;
-          this.snackBar.open(userMessage, '', {
-            duration: 3000,
-            panelClass: ['login-snackbar']
-          });
+          this.$errorMessage.next(userMessage);
         });
   }
 
-  private convertToNgo(): NewNgo {
+  private showErrorMessage(): void {
+    this.snackBar.open(this.$errorMessage.getValue(), '', {
+      duration: 3000,
+      panelClass: ['login-snackbar']
+    });
+  }
+
+  private convertFormInputToNgo(): NewNgo {
     return {
       ngoName: this.group.get('ngoNameControl')?.value,
       ngoCountry: this.group.get('countryControl')?.value,
